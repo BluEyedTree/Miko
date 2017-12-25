4517f447317aa00b6205446ae3186ba10d9d957f
Thomas Bekman <Thomas.Bekman@digitalglobe.com>
2017-12-24 17:01:57 -0700

    Added new icons for the initial grid screen

--- AdaptiveWeather/app/src/main/java/com/raywenderlich/adaptiveweather/ResultScreen.java
+++ AdaptiveWeather/app/src/main/java/com/raywenderlich/adaptiveweather/ResultScreen.java
@@ -1,14 +1,27 @@
 package com.raywenderlich.adaptiveweather;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.flexbox.FlexboxLayout;
 
@@ -18,6 +31,8 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,11 +41,41 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class ResultScreen extends AppCompatActivity {
 
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("TEST");
         setContentView(R.layout.result);
+        TextView btn = (TextView) findViewById(R.id.DistanceText);
+
+        btn.setPaintFlags(btn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        btn.setTextColor(Color.parseColor("#3366BB"));
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/search/?api=1&query=centurylink+field"));
+                startActivity(browserIntent);
+            }
+        });
+
+
 
 
     }
+
+    public static Drawable drawableFromUrl(String url) throws IOException {
+        Bitmap x;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.connect();
+        InputStream input = connection.getInputStream();
+
+        x = BitmapFactory.decodeStream(input);
+        System.out.println(x);
+        return new BitmapDrawable(x);
+    }
+
+
 }
