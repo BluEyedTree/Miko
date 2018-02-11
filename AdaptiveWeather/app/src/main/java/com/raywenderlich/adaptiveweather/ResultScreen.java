bfa0cf33ae53be1083fbcde16bf7ae06dba3a093
Thomas Bekman <Thomas.Bekman@digitalglobe.com>
2018-02-10 19:03:53 -0700

    Modified the layout, to properly show the maps fragment in results.

--- AdaptiveWeather/app/src/main/java/com/raywenderlich/adaptiveweather/ResultScreen.java
+++ AdaptiveWeather/app/src/main/java/com/raywenderlich/adaptiveweather/ResultScreen.java
@@ -1,5 +1,6 @@
 package com.raywenderlich.adaptiveweather;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -23,7 +24,25 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.constant.AvoidType;
+import com.akexorcist.googledirection.model.Direction;
+import com.akexorcist.googledirection.model.Leg;
+import com.akexorcist.googledirection.model.Route;
+import com.akexorcist.googledirection.model.Step;
+import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.flexbox.FlexboxLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,21 +54,27 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
-public class ResultScreen extends AppCompatActivity {
+public class ResultScreen extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    private static Context context;
+    public GoogleMap googleMapClass;
 
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ResultScreen.context = getApplicationContext();
         System.out.println("TEST");
         setContentView(R.layout.result);
         TextView btn = (TextView) findViewById(R.id.DistanceText);
 
-        btn.setPaintFlags(btn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+       // btn.setPaintFlags(btn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         btn.setTextColor(Color.parseColor("#3366BB"));
         btn.setOnClickListener(new View.OnClickListener()
         {
@@ -59,6 +84,10 @@ public class ResultScreen extends AppCompatActivity {
                 startActivity(browserIntent);
             }
         });
+        System.out.println("MADE IT THROUGH ON CREATE");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
 
 
@@ -77,5 +106,15 @@ public class ResultScreen extends AppCompatActivity {
         return new BitmapDrawable(x);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //googleMap  mMap = googleMap;
+
+        //// TODO: Add the API calls here so the position of the restaurant or w/e is shown on the map
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 
 }
